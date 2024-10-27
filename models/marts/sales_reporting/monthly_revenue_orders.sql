f3b1889d9b0bb0d18636b0c07c4230e7b393ca14
Abhishek Khandge <87657116+Termu-101@users.noreply.github.com>
2024-10-27 15:34:51 -0400

    Update monthly_revenue_orders.sql

--- models/marts/sales_reporting/monthly_revenue_orders.sql
+++ models/marts/sales_reporting/monthly_revenue_orders.sql
@@ -1,1 +1,17 @@
+# Answer for Exercise 3
+
+WITH monthly_sales AS (
+    SELECT
+        DATE_TRUNC('month', order_date) AS month,
+        COUNT(order_id) AS total_orders,
+        SUM(total_price) AS total_revenue
+    FROM {{ ref('stg_sales') }}
+    GROUP BY month
+)
+SELECT
+    TO_CHAR(month, 'YYYY-MM') AS month,
+    total_orders,
+    total_revenue
+FROM monthly_sales
+ORDER BY month;
 
